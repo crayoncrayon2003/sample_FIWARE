f467aeeb82152a52ae9a5153e4eb70e315db4ce1
crayoncrayon2003 <crayon.crayon2003@gmail.com>
2024-11-08 00:21:08 +0900

    main

--- sample3_OrionMulti/FiwareAPI.py
+++ sample3_OrionMulti/FiwareAPI.py
@@ -7,40 +7,80 @@ class FiwareAPI():
             self.baseURL = 'http://0.0.0.0:1026'
         else:
             self.baseURL = baseURL
-        self.SERVICE = Service
-        self.SERVICEPATH = ServicePath
-        self.HEADERS={}
-        self.setFiwareService(Service)
-        self.setFiwareServicePath(ServicePath)
 
-    def setFiwareService(self, Service):
-        self.HEADERS['Fiware-Service']=Service
+        __HEADERS={
+            'Fiware-Service'    : Service,
+            'Fiware-ServicePath': ServicePath
+        }
+        __METHODS_TABLE = {
+            # method name                           : return value
+            "getVersion"                            : lambda self: ["/version",                                 "get",     None],
+            "getStatistics"                         : lambda self: ["/statistics",                              "get",     None],
+            "deleteStatistics"                      : lambda self: ["/statistics",                              "delete",  None],
+            "getCacheStatistics"                    : lambda self: ["/cache/statistics",                        "get",     None],
+            "deleteCacheStatistics"                 : lambda self: ["/cache/statistics",                        "delete",  None],
+            "getAdminLog"                           : lambda self: ["/admin/log",                               "get",     None],
+            "putAdminLog"                           : lambda self: ["/admin/log",                               "put",     None],
+            "getEntities"                           : lambda self: ["/v2/entities/",                            "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "postEntities"                          : lambda self: ["/v2/entities/",                            "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "putEntities"                           : lambda self: ["/v2/entities/",                            "put",     {**__HEADERS, **{'Content-Type':'text/plain'      }} ],
+            "patchEntities"                         : lambda self: ["/v2/entities/",                            "patch",   {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "getTypes"                              : lambda self: ["/v2/types/",                               "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "getSubscriptions"                      : lambda self: ["/v2/subscriptions/",                       "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "postSubscriptions"                     : lambda self: ["/v2/subscriptions/",                       "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "deleteSubscriptions"                   : lambda self: ["/v2/subscriptions/",                       "delete",  __HEADERS, ],
+            "getRegistrations"                      : lambda self: ["/v2/registrations/",                       "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "postRegistrations"                     : lambda self: ["/v2/registrations/",                       "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "deleteRegistrations"                   : lambda self: ["/v2/registrations/",                       "delete",  __HEADERS, ],
+            "postOpUpdate"                          : lambda self: ["/v2/op/update/",                           "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "postOpQuery"                           : lambda self: ["/v2/op/query/",                            "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "postOpNotify"                          : lambda self: ["/v2/op/notify/",                           "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "postNotify"                            : lambda self: ["/v2/notify",                               "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "getHealth"                             : lambda self: ["/health",                                  "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "postSubscribe"                         : lambda self: ["/v2/subscribe",                            "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "postManagementConfig"                  : lambda self: ["/management/config/",                      "post",    {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "getManagementQueueNotifications"       : lambda self: ["/management/queue/notifications",          "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "deleteManagementQueueNotifications"    : lambda self: ["/management/queue/notifications",          "delete",  {**__HEADERS, **{'Content-Type':'application/json'}} ],
+            "getManagementQueueNotificationsSummary": lambda self: ["/management/queue/notifications/summary",  "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+            "getManagementQueueNotificationsCount"  : lambda self: ["/management/queue/notifications/count",    "get",     {**__HEADERS, **{'Accept'      :'application/json'}} ],
+        }
 
-    def setFiwareServicePath(self, ServicePath):
-        self.HEADERS['Fiware-ServicePath']=ServicePath
+        for name, method in __METHODS_TABLE.items():
+            # Decorate method
+            decorated_method = self.__httpReq(method)
+            # setting method to class
+            setattr(self.__class__, name, decorated_method)
 
     def printResponse(self,resp):
         print("REST API Response : ")
         print(resp)
 
     def printJsonString(self,body):
-        print("REST API Body : ")
+        print("REST API Response Body : ")
         try:
             print(json.dumps(json.loads(body), indent=2))
         except Exception as e:
             print("empty")
 
-    def __makeURI(self, path, urn):
-        uri = self.baseURL + path
-        if((urn!=None) and (len(urn)!=0)):
-            uri = self.baseURL + path + urn
-        return uri
+    # Decorator for each method
+    def __httpReq(self, func):
+        def makeURI(path, urn):
+            uri = self.baseURL + path
+            if((urn!=None) and (len(urn)!=0)):
+                uri = self.baseURL + path + urn
+            return uri
 
-    def __httpReq(func):
-        def wrapper(self, query=None, urn=None, body=None):
-            [method, path, headers] = func(self)
-            url = self.__makeURI(path, urn)
+        def wrapper(*args, **kwargs):
+            # get param
+            query = kwargs.get('query')
+            urn = kwargs.get('urn')
+            body = kwargs.get('body')
 
+            # call method of __METHODS_TABLE
+            [path, method, headers] = func(self)
+            url = makeURI(path, urn)
+
+            # call REST API
             if(body==None):
                 response = requests.request(method, url=url, headers=headers, params=query, timeout=5)
             else:
@@ -49,159 +89,3 @@ class FiwareAPI():
             return [response.ok, response.text]
 
         return wrapper
-
-    # version
-    @__httpReq
-    def getVersion(self):
-        path = "/version"
-        return ["get", path, None]
-
-    # statistics
-    @__httpReq
-    def getStatistics(self):
-        path = "/statistics"
-        return ["get", path, None]
-
-    @__httpReq
-    def deleteStatistics(self):
-        path = "/statistics"
-        return ["delete", path, None]
-
-    @__httpReq
-    def getCacheStatistics(self):
-        path = "/cache/statistics"
-        return ["get", path, None]
-
-    @__httpReq
-    def deleteCacheStatistics(self):
-        path = "/cache/statistics"
-        return ["delete", path, None]
-
-    # log
-    @__httpReq
-    def getAdminLog(self):
-        path = "/admin/log"
-        return ["get", path, None]
-
-    @__httpReq
-    def putAdminLog(self):
-        path = "/admin/log"
-        return ["put", path, None]
-
-    # entities
-    @__httpReq
-    def postEntities(self):
-        path = "/v2/entities/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
-    @__httpReq
-    def putEntities(self):
-        path = "/v2/entities/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'text/plain'
-
-        return ["put", path, headers]
-
-    @__httpReq
-    def patchEntities(self):
-        path = "/v2/entities/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["patch", path, headers]
-
-    @__httpReq
-    def getEntities(self):
-        path = "/v2/entities/"
-        headers = self.HEADERS
-        headers['Accept'] = 'application/json'
-
-        return ["get", path, headers]
-
-    # types
-    @__httpReq
-    def getTypes(self):
-        path = "/v2/types/"
-        headers = self.HEADERS
-        headers['Accept'] = 'application/json'
-
-        return ["get", path, headers]
-
-    # subscriptions
-    @__httpReq
-    def getSubscriptions(self):
-        path = "/v2/subscriptions/"
-        headers = self.HEADERS
-        headers['Accept'] = 'application/json'
-
-        return ["get", path, headers]
-
-    @__httpReq
-    def postSubscriptions(self):
-        path = "/v2/subscriptions/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
-    @__httpReq
-    def deleteSubscriptions(self):
-        path = "/v2/subscriptions/"
-        headers = self.HEADERS
-
-        return ["delete", path, headers]
-
-    # registrations
-    @__httpReq
-    def getRegistrations(self):
-        path = "/v2/registrations/"
-        headers = self.HEADERS
-        headers['Accept'] = 'application/json'
-
-        return ["get", path, headers]
-
-    @__httpReq
-    def postRegistrations(self):
-        path = "/v2/registrations/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
-    @__httpReq
-    def deleteRegistrations(self):
-        path = "/v2/registrations/"
-        headers = self.HEADERS
-
-        return ["delete", path, headers]
-
-    # op/update
-    @__httpReq
-    def postUpdate(self):
-        path = "/v2/op/update/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
-    # op/query
-    @__httpReq
-    def postOpQuery(self):
-        path = "/v2/op/query/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
-    # op/notify
-    @__httpReq
-    def postNotify(self):
-        path = "/v2/op/notify/"
-        headers = self.HEADERS
-        headers['Content-Type'] = 'application/json'
-
-        return ["post", path, headers]
-
