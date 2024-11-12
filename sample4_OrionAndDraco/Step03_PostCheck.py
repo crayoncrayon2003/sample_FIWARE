da42a79ef6208568a5041d36643ee913f6471954
crayoncrayon2003 <crayon.crayon2003@gmail.com>
2024-11-12 21:41:39 +0900

    main

--- sample4_OrionAndDraco/Step03_PostCheck.py
+++ sample4_OrionAndDraco/Step03_PostCheck.py
@@ -1,6 +1,6 @@
 def main():
     print("Access the http://localhost:8080/nifi/ using the Web browser.")
-    print("And, try double clicking the Block \"test_process_group\".")
+    print("And, try double clicking the Block \"flow1\".")
     print("left  : this flow execute onece time at startup")
     print("right : This flow execute periodically")
 
