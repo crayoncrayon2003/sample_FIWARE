d3ca6d6aff662c318dc1a6488609f1bb28eb3423
crayoncrayon2003 <crayon.crayon2003@gmail.com>
2024-12-23 18:58:37 +0900

    main

--- sample5_OrionAndCygnus/sample3_RESTCygnusOrion/flume-custom-source-rest/project/src/main/java/org/example/flume/custom/source/CustomSource.java
+++ sample5_OrionAndCygnus/sample3_RESTCygnusOrion/flume-custom-source-rest/project/src/main/java/org/example/flume/custom/source/CustomSource.java
@@ -1,94 +1,126 @@
 package org.example.flume.custom.source;
 
 import org.apache.flume.*;
-import org.apache.flume.Context;
-import org.apache.flume.Event;
-import org.apache.flume.event.EventBuilder;
-import org.apache.flume.source.AbstractSource;
 import org.apache.flume.conf.Configurable;
-import org.slf4j.Logger;
+import org.apache.flume.event.SimpleEvent;
+import org.apache.flume.source.AbstractSource;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.json.JSONObject;
+import java.util.Random;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-public class CustomSource extends AbstractSource implements Configurable {
-    private static final Logger LOG = LoggerFactory.getLogger(CustomSource.class);
-    private String apiUrl;
-    private long pollingInterval;
-    private volatile boolean running;
-
-    public static final String INPUT_HOST = "input.host";
-    public static final String INPUT_PORT = "input.port";
-    public static final String INPUT_INTERVAL = "input.interval";
+public class CustomSourceRest extends AbstractSource implements Configurable, PollableSource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomSourceRest.class);
+    private Random random;
 
     @Override
     public void configure(Context context) {
-        LOG.debug("[CustomSource][configure] start");
-        String HOST = context.getString(INPUT_HOST, "localhost");
-        String PORT = context.getString(INPUT_PORT, "8080");
-        apiUrl = String.format("http://%s:%s", HOST, PORT);
-        pollingInterval = context.getLong(INPUT_INTERVAL, 10000L); // Default to 10 seconds
-        LOG.debug("[CustomSource][configure] end");
+        // load config
+        LOGGER.debug("[CustomSourceRest][configure] start");
+        this.random = new Random();
+        LOGGER.debug("[CustomSourceRest][configure] end");
     }
 
     @Override
-    public synchronized void start() {
-        LOG.debug("[CustomSource][start] start");
-        running = true;
-        LOG.debug("CustomSource started with API URL: {} and polling interval: {} ms", apiUrl, pollingInterval);
-        new Thread(() -> {
-            while (running) {
-                try {
-                    pollApi();
-                    Thread.sleep(pollingInterval);
-                } catch (InterruptedException e) {
-                    LOG.error("Interrupted while sleeping", e);
-                    running = false;
-                } catch (IOException e) {
-                    LOG.error("Error polling API", e);
-                }
-            }
-        }).start();
+    public void start() {
+        LOGGER.debug("[CustomSourceRest][start] start");
         super.start();
-        LOG.debug("[CustomSource][start] end");
+        LOGGER.debug("[CustomSourceRest][start] end");
     }
 
     @Override
-    public synchronized void stop() {
-        LOG.debug("[CustomSource][stop] start");
-        running = false;
-        LOG.debug("CustomSource stopped");
+    public void stop() {
+        LOGGER.debug("[CustomSourceRest][stop] start");
         super.stop();
-        LOG.debug("[CustomSource][stop] end");
+        LOGGER.debug("[CustomSourceRest][stop] end");
     }
 
-    private void pollApi() throws IOException {
-        LOG.debug("[CustomSource][pollApi] start");
-        URL url = new URL(apiUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        int responseCode = conn.getResponseCode();
 
-        if (responseCode == 200) {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                // Log the retrieved data
-                LOG.info("Data retrieved from API: {}", content.toString());
-                // Create a Flume event with the data
-                Event event = EventBuilder.withBody(content.toString().getBytes());
-                getChannelProcessor().processEvent(event);
-            }
-        } else {
-            LOG.error("Failed to poll API: HTTP response code {}", responseCode);
+    // first failure, the wait time is  2 seconds.(0+2)
+    // next  failure, the wait time is  4 seconds.(2+2)
+    // next  failure, the wait time is  6 seconds.(4+2)
+    // next  failure, the wait time is  8 seconds.(6+2)
+    // next  failure, the wait time is 10 seconds.(8+2)
+    // next  failure, the wait time is 10 seconds.(maximum) ...
+    // next  failure, the wait time is 10 seconds.(maximum) ...
+    @Override public long getBackOffSleepIncrement() {
+        // This method determines the amount of time (milliseconds)
+        // to increase the wait time between each retry.
+        // In this example, it is set to 2,000 milliseconds (2 seconds).
+        return 2000L;
+    }
+    @Override public long getMaxBackOffSleepInterval() {
+        // This method sets the maximum amount of time (in milliseconds) to wait between retries.
+        // In this example, it is set to 10,000 milliseconds (10 seconds).
+        return 10000L;
+    }
+
+    @Override
+    public Status process() throws EventDeliveryException {
+        LOGGER.debug("[CustomSourceRest][process] start");
+        Status status = null;
+
+        try {
+            Event event = new SimpleEvent();
+
+            // create event data
+            // ex.
+            // {
+            //     "name": {
+            //         "type": "Text",
+            //         "value": "This is CustomSourceConst",
+            //         "metadata": {}
+            //     },
+            //     "temperature": {
+            //         "type": "Integer",
+            //         "value": 42,  // example value
+            //         "metadata": {}
+            //     },
+            //     "humidity": {
+            //         "type": "Integer",
+            //         "value": 73,  // example value
+            //         "metadata": {}
+            //     }
+            // }
+
+            int temperature = this.random.nextInt(101);
+            int humidity    = this.random.nextInt(101);
+
+            JSONObject jsonName = new JSONObject();
+            jsonName.put("type", "Text");
+            jsonName.put("value", "This is CustomSourceRest");
+            jsonName.put("metadata", new JSONObject());
+
+            JSONObject jsonTemperature = new JSONObject();
+            jsonTemperature.put("type", "Integer");
+            jsonTemperature.put("value", temperature);
+            jsonTemperature.put("metadata", new JSONObject());
+
+            JSONObject jsonHumidity = new JSONObject();
+            jsonHumidity.put("type", "Integer");
+            jsonHumidity.put("value", humidity);
+            jsonHumidity.put("metadata", new JSONObject());
+
+            JSONObject json = new JSONObject();
+            json.put("name",        jsonName);
+            json.put("temperature", jsonTemperature);
+            json.put("humidity",    jsonHumidity);
+
+            // set data to event
+            event.setBody(json.toString().getBytes());
+
+            // send event
+            getChannelProcessor().processEvent(event);
+
+            status = Status.READY;
+        } catch (ChannelFullException e) {
+            LOGGER.error("[CustomSourceRest][process] end. Channel is full, backing off", e);
+            status = Status.BACKOFF;
+        } catch (Throwable t) {
+            LOGGER.error("[CustomSourceRest][process] end. Error processing event", t);
+            status = Status.BACKOFF;
         }
-        LOG.debug("[CustomSource][pollApi] end");
+        LOGGER.debug("[CustomSourceRest][process] end");
+        return status;
     }
 }
